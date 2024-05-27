2bc00d2e02e6c19853102977d2c1469de33e9437
Alif <alif.972002@gmail.com>
2024-05-27 14:20:34 +0600

    updated UI

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -7,17 +7,17 @@ function Hours() {
       <section className="bg-white h-screen flex flex-col items-center justify-center p-8">
         <div className="w-full max-w-2xl">
           <h2 className="text-6xl font-medium mb-10 text-center">DINING HOURS</h2>
-          <div className="mb-8 p-4 bg-[#F9F6EF] rounded drop-shadow w-full">
+          <div className="mb-8 p-4 bg-[#F9F6EF] rounded-[24px] drop-shadow w-full">
             <h3 className="text-2xl mb-2">LUNCH HOURS</h3>
-            <p>MONDAY - CLOSED</p>
-            <p>TUESDAY - FRIDAY - 11:00 AM to 3:00 PM</p>
+            <p>MONDAY : CLOSED</p>
+            <p>TUESDAY - FRIDAY : 11:00 AM to 3:00 PM</p>
           </div>
-          <div className="p-4 bg-[#F9F6EF] rounded drop-shadow w-full">
+          <div className="p-4 bg-[#F9F6EF] rounded-[24px] drop-shadow w-full">
             <h3 className="text-2xl mb-2">DINNER HOURS</h3>
-            <p>MONDAY - CLOSED</p>
-            <p>TUESDAY - FRIDAY - 5:00 PM to 10PM</p>
-            <p>SATURDAY - 12:00 PM to 10PM</p>
-            <p>SUNDAY - 12:00PM to 9:00PM</p>
+            <p>MONDAY : CLOSED</p>
+            <p>TUESDAY - FRIDAY : 5:00 PM to 10PM</p>
+            <p>SATURDAY : 12:00 PM to 10PM</p>
+            <p>SUNDAY : 12:00PM to 9:00PM</p>
           </div>
         </div>
       </section>
